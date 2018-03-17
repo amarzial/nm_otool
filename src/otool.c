abe8648d8862e688e25670e9ccb6db77a741bf25
Alessandro Marzialetti <setti93@gmail.com>
2018-03-17 12:07:31 +0100

    wip

--- src/otool.c
+++ src/otool.c
@@ -6,11 +6,10 @@
 /*   By: amarzial <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/26 14:09:47 by amarzial          #+#    #+#             */
-/*   Updated: 2018/03/15 12:56:16 by amarzial         ###   ########.fr       */
+/*   Updated: 2018/03/17 12:06:28 by amarzial         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "header.h"
 #include "internal.h"
 #include "libft.h"
 
@@ -32,24 +31,24 @@ void print_command_64(t_command64 *c, size_t of)
 
     ft_memcpy(segname, c->segname, 16);
     segname[16] = '\0';
-    ft_printf("Command:%#x\n  cmd: %u\n  cmdsize: %u\n  \
+    ft_printf("Command:%#x\n  cmd: %#x\n  cmdsize: %u\n  \
 segname: %s\n  vmaddr: %llu\n  vmsize: %llu\n  fileoff: %llu\n  \
-filesize: %llu\n  maxprot: %u\n  initprot: %u\n  nsects: %u\n  flags: %u\n", of,
-              c->cmd, c->cmdsize, segname, c->vmaddr, c->vmsize, c->fileoff, c->filesize, c->maxprot, c->initprot, c->nsects, c->flags);
+filesize: %llu\n  maxprot: %u\n  initprot: %u\n  nsects: %u\n  flags: %u\n",
+              of, c->cmd, c->cmdsize, segname, c->vmaddr, c->vmsize, c->fileoff, c->filesize, c->maxprot, c->initprot, c->nsects, c->flags);
 }
 
 t_segment64 *get_text_segment(t_command64 *c)
 {
-    t_segment64     *seg;
-    unsigned int    i;
+    t_segment64 *seg;
+    unsigned int i;
 
     i = 0;
     seg = NULL;
     while (i < c->nsects)
     {
-        seg = (void*)((char*)c + sizeof(t_command64) + (i * sizeof(t_segment64)));
+        seg = (void *) ((char *) c + sizeof(t_command64) + (i * sizeof(t_segment64)));
         ft_printf("sectname: %.16s, segname: %.16s\n", seg->sectname, seg->segname);
-                    ++i;
+        ++i;
     }
     return seg;
 }
@@ -57,8 +56,8 @@ t_segment64 *get_text_segment(t_command64 *c)
 int main(int argc, char **argv)
 {
     t_file_map   map;
-    t_header64   *h64;
-    t_command64  *s64;
+    t_header64 * h64;
+    t_command64 *s64;
     unsigned int cmds;
     size_t       offset;
 
@@ -75,12 +74,11 @@ int main(int argc, char **argv)
             while (cmds < h64->ncmds)
             {
                 s64 = (void *) ((char *) map.ptr + offset);
+                print_command_64(s64, offset);
                 if (s64->cmd == LC_SEGMENT_64)
                 {
                     get_text_segment(s64);
-                    break;
                 }
-                print_command_64(s64, offset);
                 offset += s64->cmdsize;
                 ++cmds;
             }
